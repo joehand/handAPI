9d13e8c1efdc6b4c9783fdf77e07ce976bb97e81
Joe Hand <joe@joehand.org>
2013-12-04 07:16:31 -0700

    finish class views for fitbit

--- hand_api/services/api_views.py
+++ hand_api/services/api_views.py
@@ -1,46 +1,48 @@
-from flask import Blueprint, render_template, url_for, flash, redirect, session, request, jsonify
+from flask import (Blueprint, render_template, url_for, flash, 
+                    redirect, session, request, jsonify)
+from flask.views import View
 from flask.ext.security import login_required, current_user
 
-class API_View():
 
-    api = None
-    blueprint = None
-    name = ''
+class APIRootView(View):
+    decorators = [login_required]
+    
+    def __init__(self, blueprint):
+        self.blueprint = blueprint
+
+    def dispatch_request(self):
+        print self.blueprint.name
+        if current_user.get(self.blueprint.name, None):
+            return redirect(url_for('frontend.index'))
+        return self.blueprint.oauth.authorize(callback=url_for('.authorized', _external=True))
+
+class APIAuthorizedView(View):
+    decorators = [login_required]
 
     def __init__(self, blueprint):
-        blueprint = blueprint
-        api = blueprint.api
-        name = blueprint.name
+        self.blueprint = blueprint
 
-        print name
-        print blueprint.api
-    
-    @bp.route('/')
-    @login_required
-    def login():
-        if current_user.get(bp.name, None):
-            return redirect(url_for('frontend.index'))
-        return bp.oauth.authorize(callback=url_for('.authorized', _external=True))
-
-    @bp.route('/authorized')
-    @bp.oauth.authorized_handler
-    def authorized(resp):
+    def dispatch_request(self, resp):
+        print self.blueprint.name
         if resp is None:
             flash(u'You denied the request to sign in.')
             return redirect(url_for('frontend.index'))
             
-        if bp.oauth_type == 'oauth2':
+        if self.blueprint.api.oauth_type == 'oauth2':
             resp['access_token'] = (resp['access_token'], '') #need to make it a tuple for oauth2 requests
 
-        current_user[bp.name] = resp
+        current_user[self.blueprint.name] = resp
         current_user.save()
 
-        flash('You were signed in to %s' % bp.name.capitalize())
+        flash('You were signed in to %s' % self.blueprint.name.capitalize())
         return redirect(url_for('frontend.index'))
 
-    @bp.oauth.tokengetter
-    def get_token(token=None):
-        if bp.oauth_type == 'oauth2':
-            return current_user.get(bp.name, None)['access_token']
-        return current_user.get(bp.name, None)['oauth_token']
-        
+def registerAPIViews(blueprint):
+    root_view = APIRootView.as_view('login', blueprint=blueprint)
+    blueprint.add_url_rule('/', view_func=root_view)
+
+    auth_view = blueprint.oauth.authorized_handler(
+                        APIAuthorizedView.as_view('authorized', blueprint=blueprint))
+    blueprint.add_url_rule('/authorized', view_func=auth_view)
+
+    return blueprint
--- hand_api/services/fitbit/views.py
+++ hand_api/services/fitbit/views.py
@@ -1,9 +1,9 @@
 from flask import (Blueprint, render_template, url_for, flash, 
                     redirect, session, request, jsonify)
-from flask.views import View
 from flask.ext.security import login_required, current_user
 
 from .api import FitbitAPI
+from ..api_views import registerAPIViews
 from ...user import User
 
 fitbit = Blueprint('fitbit', __name__, url_prefix='/fitbit')
@@ -12,42 +12,10 @@ bp = fitbit
 bp.api = FitbitAPI()
 bp.oauth = bp.api.oauth_app
 
-class APIRootView(View):
-    decorators = [login_required]
-
-    def dispatch_request(self):
-        if current_user.get(bp.name, None):
-            return redirect(url_for('frontend.index'))
-        return bp.oauth.authorize(callback=url_for('.authorized', _external=True))
-
-class APIAuthorizedView(View):
-    decorators = [login_required]
-
-    def dispatch_request(self, resp):
-        if resp is None:
-            flash(u'You denied the request to sign in.')
-            return redirect(url_for('frontend.index'))
-            
-        if bp.api.oauth_type == 'oauth2':
-            resp['access_token'] = (resp['access_token'], '') #need to make it a tuple for oauth2 requests
-
-        current_user[bp.name] = resp
-        current_user.save()
-
-        flash('You were signed in to %s' % bp.name.capitalize())
-        return redirect(url_for('frontend.index'))
-
-
-root_view = APIRootView.as_view('login')
-bp.add_url_rule('/', view_func=root_view)
-
-auth_view = bp.oauth.authorized_handler(APIAuthorizedView.as_view('authorized'))
-bp.add_url_rule('/authorized', view_func=auth_view)
-
+registerAPIViews(bp)
 
 @bp.oauth.tokengetter
 def get_token(token=None):
     if bp.api.oauth_type == 'oauth2':
         return current_user.get(bp.name, None)['access_token']
     return current_user.get(bp.name, None)['oauth_token']
-    
